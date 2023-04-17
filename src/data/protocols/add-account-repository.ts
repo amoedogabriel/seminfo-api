519db962fa43f3074f3d4e3edb8f5263c920baa8
Breno Gonzaga <contato@gonzaga.dev.br>
2023-04-17 18:19:03 -0400

    refactor: move add account repository to db folder

--- src/data/protocols/add-account-repository.ts
+++
@@ -1,6 +0,0 @@
-import { AccountModel } from '../../domain/models/account';
-import { AddAccountModel } from '../../domain/models/add-account';
-
-export interface AddAccountRepository {
-  addAccount(account: AddAccountModel): Promise<AccountModel>;
-}
