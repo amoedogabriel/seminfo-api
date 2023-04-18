22addfbd096b466cda35b61ada524dc340c0735d
Breno Gonzaga <contato@gonzaga.dev.br>
2023-04-17 22:13:44 -0400

    feat: add mongo helper methods

---
+++ src/infra/helper/mongo-helper.ts
@@ -0,0 +1,24 @@
+import { Collection, MongoClient } from 'mongodb';
+
+export const MongoHelper = {
+  client: null as MongoClient,
+  uri: null as string,
+  async connect(uri: string): Promise<void> {
+    this.uri = uri;
+    this.client = await MongoClient.connect(uri);
+  },
+  async disconnect(): Promise<void> {
+    await this.client.close();
+  },
+  async getCollection(name: string): Promise<Collection> {
+    return this.client.db().collection(name);
+  },
+  standardizeId(data: any) {
+    const id = data._id;
+    delete data._id;
+    return {
+      id,
+      ...data,
+    };
+  },
+};
