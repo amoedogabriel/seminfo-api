40eeb2cacbc7a3ac8e168aa5b3043a7f07e81175
Breno Gonzaga <contato@gonzaga.dev.br>
2023-04-17 22:13:57 -0400

    test: ensure mongodb reconnect if is down

---
+++ tests/helper/mongo-helper.unit.ts
@@ -0,0 +1,20 @@
+import { MongoHelper } from '../../src/infra/helper/mongo-helper';
+
+describe('Mongo Helper', () => {
+  const sut = MongoHelper;
+  beforeAll(async () => {
+    await MongoHelper.connect('mongodb://root:root@localhost:27017/');
+  });
+
+  afterAll(async () => {
+    await sut.disconnect();
+  });
+
+  it('Should reconnect if mongodb is down', async () => {
+    let accountCollection = await sut.getCollection('account');
+    expect(accountCollection).toBeTruthy();
+    await sut.disconnect();
+    accountCollection = await sut.getCollection('account');
+    expect(accountCollection).toBeTruthy();
+  });
+});
