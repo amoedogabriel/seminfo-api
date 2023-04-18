81ed5191a24889d24b464266afb0d0d87aeb3162
Breno Gonzaga <contato@gonzaga.dev.br>
2023-04-18 00:44:28 -0400

    feat: ensure BCryptAdapter calls hash bcrypt with correct value

---
+++ src/infra/cryptography/bcrypt-adapter.ts
@@ -0,0 +1,13 @@
+import { Hasher } from '../../data/protocols/cryptography/hasher';
+import bcrypt from 'bcrypt';
+
+export class BCryptAdapter implements Hasher {
+  private readonly salt: number;
+  constructor(salt: number) {
+    this.salt = salt;
+  }
+  async hash(value: string): Promise<string> {
+    await bcrypt.hash(value, this.salt);
+    return null;
+  }
+}
