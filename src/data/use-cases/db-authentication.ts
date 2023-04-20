46db05eeae29da197616426d85b0d7b3f3cc3445
Breno Gonzaga <contato@gonzaga.dev.br>
2023-04-20 18:45:09 -0400

    feat: ensure DbAuthentication calls UpdateAccessToken with correct value

--- src/data/use-cases/db-authentication.ts
+++ src/data/use-cases/db-authentication.ts
@@ -1,18 +1,25 @@
 import { Encrypter } from '@data/protocols/cryptography';
-import { LoadAccountByEmailRepository } from '@data/protocols/db';
+import { LoadAccountByEmailRepository, UpdateAccessTokenRepository } from '@data/protocols/db';
 import { AuthenticationModel } from '@domain/models';
 import { Authentication } from '@domain/use-cases';
 
 export class DbAuthentication implements Authentication {
   private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository;
   private readonly encrypter: Encrypter;
-  constructor(loadAccountByEmailRepository: LoadAccountByEmailRepository, encrypter: Encrypter) {
+  private readonly updateAccessToken: UpdateAccessTokenRepository;
+  constructor(
+    loadAccountByEmailRepository: LoadAccountByEmailRepository,
+    encrypter: Encrypter,
+    updateAccessToken: UpdateAccessTokenRepository
+  ) {
     this.loadAccountByEmailRepository = loadAccountByEmailRepository;
     this.encrypter = encrypter;
+    this.updateAccessToken = updateAccessToken;
   }
   async auth(authentication: AuthenticationModel): Promise<string> {
     const account = await this.loadAccountByEmailRepository.loadByEmail(authentication.email);
-    await this.encrypter.encrypt(account.id);
+    const token = await this.encrypter.encrypt(account.id);
+    await this.updateAccessToken.updateAccessToken(authentication.email, token);
     return null;
   }
 }
