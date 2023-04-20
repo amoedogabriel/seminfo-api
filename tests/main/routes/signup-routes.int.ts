7b171dc2dd5d1a076c690f854abb239a3db19cc1
Breno Gonzaga <contato@gonzaga.dev.br>
2023-04-20 13:13:19 -0400

    refactor: change account params

--- tests/main/routes/signup-routes.int.ts
+++ tests/main/routes/signup-routes.int.ts
@@ -1,7 +1,6 @@
 import { MongoHelper } from '@infra/helper';
 import app from '@main/config/app';
 import env from '@main/config/env';
-import { makeFakeAccountRequest } from '@tests/helper/make-fake-account-request';
 import { Collection } from 'mongodb';
 import request from 'supertest';
 
@@ -20,7 +19,12 @@ describe('SignUp Routes', () => {
     accountCollection.deleteMany({});
   });
   it('Should return an account on success', async () => {
-    const account = makeFakeAccountRequest().body;
+    const account = {
+      name: 'any_name',
+      email: 'any_email@mail.com',
+      password: '@Valid123',
+      passwordConfirmation: '@Valid123',
+    };
     await request(app).post('/signup').send(account).expect(200);
   });
 });
