cd6769bacf484eed41995802efdfbce28e42d597
Breno Gonzaga <contato@gonzaga.dev.br>
2023-04-20 01:32:37 -0400

    feat: ensure SignUpController calls Validation with correct values

--- src/presentation/controllers/signup-controller.ts
+++ src/presentation/controllers/signup-controller.ts
@@ -1,14 +1,18 @@
 import { AddAccount } from '@domain/use-cases';
 import { serveError, ok } from '@presentation/helper/http/http-helper';
 import { Controller, HttpRequest, HttpResponse } from '@presentation/protocols';
+import { Validation } from '@presentation/protocols/validation';
 
 export class SignUpControler implements Controller {
   private readonly addAccount: AddAccount;
-  constructor(addAccount: AddAccount) {
+  private readonly validation: Validation;
+  constructor(addAccount: AddAccount, validation: Validation) {
     this.addAccount = addAccount;
+    this.validation = validation;
   }
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
+      this.validation.validate(httpRequest.body);
       const httpResponse = await this.addAccount.add(httpRequest.body);
       return ok(httpResponse);
     } catch (error) {
