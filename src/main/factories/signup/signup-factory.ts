99b40f9fe84923009ddbbe061791b4d640100c3d
Breno Gonzaga <contato@gonzaga.dev.br>
2023-04-20 11:54:50 -0400

    feat: make SignUpController composition

--- src/main/factories/signup/signup-factory.ts
+++ src/main/factories/signup/signup-factory.ts
@@ -2,11 +2,13 @@ import { DbAddAccount } from '@data/use-cases';
 import { BCryptAdapter } from '@infra/cryptography';
 import { AccountMongoRepository } from '@infra/db';
 import { SignUpControler } from '@presentation/controllers/signup-controller';
+import { makeSignupValidation } from './signup-validation-factory';
 
 export const makeSignupController = (): SignUpControler => {
   const salt = 12;
   const hasher = new BCryptAdapter(salt);
   const addAccountRepository = new AccountMongoRepository();
   const addAcount = new DbAddAccount(hasher, addAccountRepository);
-  return new SignUpControler(addAcount);
+  const validation = makeSignupValidation();
+  return new SignUpControler(addAcount, validation);
 };
