ed3a31227e3776bc704a59db7363d6f588bfbc70
Breno Gonzaga <contato@gonzaga.dev.br>
2023-04-20 01:10:01 -0400

    feat: ensure AccountMongoRepository returns on account on load by email success

--- src/infra/db/account-mongo-repository.ts
+++ src/infra/db/account-mongo-repository.ts
@@ -1,12 +1,19 @@
 import { AddAccountRepository } from '@data/protocols/db';
+import { LoadAccountByEmailRepository } from '@data/protocols/db/load-account-by-email-repository';
 import { AddAccountModel, AccountModel } from '@domain/models';
 import { MongoHelper } from '@infra/helper';
 
-export class AccountMongoRepository implements AddAccountRepository {
+export class AccountMongoRepository implements AddAccountRepository, LoadAccountByEmailRepository {
   async addAccount(account: AddAccountModel): Promise<AccountModel> {
     const accountCollection = await MongoHelper.getCollection('account');
     const accountid = await accountCollection.insertOne(account).then((data) => data.insertedId);
     const findAccount = await accountCollection.findOne(accountid);
+    return findAccount && MongoHelper.standardizeId(findAccount);
+  }
+
+  async loadByEmail(email: string): Promise<AccountModel> {
+    const accountCollection = await MongoHelper.getCollection('account');
+    const findAccount = await accountCollection.findOne({ email });
     return findAccount && MongoHelper.standardizeId(findAccount);
   }
 }
