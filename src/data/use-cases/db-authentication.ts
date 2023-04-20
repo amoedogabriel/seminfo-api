5d170da6649cafc8de5868fd17b1380ba633228c
Breno Gonzaga <contato@gonzaga.dev.br>
2023-04-20 19:26:14 -0400

    feat: ensure DbAuthentication calls HashCompare with correct values

--- src/data/use-cases/db-authentication.ts
+++ src/data/use-cases/db-authentication.ts
@@ -1,24 +1,29 @@
 import { Encrypter } from '@data/protocols/cryptography';
+import { HashCompare } from '@data/protocols/cryptography/hash-compare';
 import { LoadAccountByEmailRepository, UpdateAccessTokenRepository } from '@data/protocols/db';
 import { AuthenticationModel } from '@domain/models';
 import { Authentication } from '@domain/use-cases';
 
 export class DbAuthentication implements Authentication {
   private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository;
-  private readonly encrypter: Encrypter;
   private readonly updateAccessToken: UpdateAccessTokenRepository;
+  private readonly encrypter: Encrypter;
+  private readonly hashCompare: HashCompare;
   constructor(
     loadAccountByEmailRepository: LoadAccountByEmailRepository,
+    updateAccessToken: UpdateAccessTokenRepository,
     encrypter: Encrypter,
-    updateAccessToken: UpdateAccessTokenRepository
+    hashCompare: HashCompare
   ) {
     this.loadAccountByEmailRepository = loadAccountByEmailRepository;
-    this.encrypter = encrypter;
     this.updateAccessToken = updateAccessToken;
+    this.encrypter = encrypter;
+    this.hashCompare = hashCompare;
   }
   async auth(authentication: AuthenticationModel): Promise<string> {
     const account = await this.loadAccountByEmailRepository.loadByEmail(authentication.email);
     if (account) {
+      await this.hashCompare.compare(authentication.password, account.password);
       const token = await this.encrypter.encrypt(account.id);
       await this.updateAccessToken.updateAccessToken(authentication.email, token);
     }
