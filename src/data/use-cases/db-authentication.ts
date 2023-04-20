8ece45b0df5c5539477d26b646d4843979a06f4c
Breno Gonzaga <contato@gonzaga.dev.br>
2023-04-20 18:37:00 -0400

    feat: ensure DbAuthentication calls Encrypter with correct value

--- src/data/use-cases/db-authentication.ts
+++ src/data/use-cases/db-authentication.ts
@@ -1,14 +1,18 @@
+import { Encrypter } from '@data/protocols/cryptography';
 import { LoadAccountByEmailRepository } from '@data/protocols/db';
 import { AuthenticationModel } from '@domain/models';
 import { Authentication } from '@domain/use-cases';
 
 export class DbAuthentication implements Authentication {
   private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository;
-  constructor(loadAccountByEmailRepository: LoadAccountByEmailRepository) {
+  private readonly encrypter: Encrypter;
+  constructor(loadAccountByEmailRepository: LoadAccountByEmailRepository, encrypter: Encrypter) {
     this.loadAccountByEmailRepository = loadAccountByEmailRepository;
+    this.encrypter = encrypter;
   }
   async auth(authentication: AuthenticationModel): Promise<string> {
-    await this.loadAccountByEmailRepository.loadByEmail(authentication.email);
+    const account = await this.loadAccountByEmailRepository.loadByEmail(authentication.email);
+    await this.encrypter.encrypt(account.id);
     return null;
   }
 }
