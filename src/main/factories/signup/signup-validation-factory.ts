5dd2f3fe88ecab4890cb2908c064b5e5f512007f
Breno Gonzaga <contato@gonzaga.dev.br>
2023-04-20 13:07:11 -0400

    feat: ensure SingUpValidation factory calls ValidationComposite with correct values

--- src/main/factories/signup/signup-validation-factory.ts
+++ src/main/factories/signup/signup-validation-factory.ts
@@ -1,4 +1,8 @@
+import { EmailValidatorAdapter } from '@infra/validators/email-validator-adapter';
+import { StrongPasswordValidatorAdapter } from '@infra/validators/strong-password-validator-adapter';
 import { Validation } from '@presentation/protocols/validation';
+import { EmailValidation } from '@validation/validators/email-validation';
+import { PasswordValidation } from '@validation/validators/password-validation';
 import { RequiredFieldValidation } from '@validation/validators/required-field-validation';
 import { ValidationComposite } from '@validation/validators/validation-composite';
 
@@ -8,6 +12,7 @@ export const makeSignupValidation = (): ValidationComposite => {
   for (const field of requiredFields) {
     validations.push(new RequiredFieldValidation(field));
   }
-
+  validations.push(new EmailValidation('email', new EmailValidatorAdapter()));
+  validations.push(new PasswordValidation('password', new StrongPasswordValidatorAdapter()));
   return new ValidationComposite(validations);
 };
