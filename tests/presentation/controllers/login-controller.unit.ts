b07997208c7fa7a8172ace843dc8280ff7c1a2d6
Breno Gonzaga <contato@gonzaga.dev.br>
2023-04-20 21:57:21 -0400

    test: ensure LoginController calls Authentication with correct values

--- tests/presentation/controllers/login-controller.unit.ts
+++ tests/presentation/controllers/login-controller.unit.ts
@@ -1,17 +1,20 @@
 import { LoginController } from '@presentation/controllers/login-controller';
 import { Validation } from '@presentation/protocols';
-import { ValidationStub } from '../test';
+import { AuthenticationStub, ValidationStub } from '../test';
 import { makeFakeAuthenticationRequest } from '@tests/helper';
+import { Authentication } from '@domain/use-cases';
 
 type SutTypes = {
   sut: LoginController;
   validation: Validation;
+  authentication: Authentication;
 };
 
 const makeSut = (): SutTypes => {
   const validation = new ValidationStub();
-  const sut = new LoginController(validation);
-  return { sut, validation };
+  const authentication = new AuthenticationStub();
+  const sut = new LoginController(validation, authentication);
+  return { sut, validation, authentication };
 };
 
 describe('LoginController', () => {
@@ -20,5 +23,12 @@ describe('LoginController', () => {
     const validationSpy = jest.spyOn(validation, 'validate');
     await sut.handle(makeFakeAuthenticationRequest());
     expect(validationSpy).toHaveBeenCalledWith(makeFakeAuthenticationRequest().body);
+  });
+
+  it('Should call Authentication with correct values', async () => {
+    const { sut, authentication } = makeSut();
+    const authenticationSpy = jest.spyOn(authentication, 'auth');
+    await sut.handle(makeFakeAuthenticationRequest());
+    expect(authenticationSpy).toHaveBeenCalledWith(makeFakeAuthenticationRequest().body);
   });
 });
