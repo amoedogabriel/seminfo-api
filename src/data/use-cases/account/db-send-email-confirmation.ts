20f42946d8818046fe4b1fe674c74c3c0f7b7797
Breno Gonzaga <contato@gonzaga.dev.br>
2023-04-21 19:17:18 -0400

    feat: ensure DbSendEmailConfirmation calls SetEmailConfirmationTokenRepository with correct email

--- src/data/use-cases/account/db-send-email-confirmation.ts
+++ src/data/use-cases/account/db-send-email-confirmation.ts
@@ -1,22 +1,26 @@
 import { LoadAccountByEmailRepository } from '@data/protocols/db/account';
 import { SendEmailConfirmationRepository } from '@data/protocols/db/account/send-email-confirmation-repository';
-import { SendEmailConfirmationModel } from '@domain/models/send-email';
+import { SetEmailConfirmationTokenRepository } from '@data/protocols/db/send-email';
 import { SendEmailConfirmation } from '@domain/use-cases/send-email';
 
 export class DbSendEmailConfirmation implements SendEmailConfirmation {
   private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository;
+  private readonly setEmailConfirmationTokenRepository: SetEmailConfirmationTokenRepository;
   private readonly sendEmailConfirmationRepository: SendEmailConfirmationRepository;
   constructor(
     loadAccountByEmailRepository: LoadAccountByEmailRepository,
+    setEmailConfirmationTokenRepository: SetEmailConfirmationTokenRepository,
     sendEmailConfirmationRepository: SendEmailConfirmationRepository
   ) {
     this.loadAccountByEmailRepository = loadAccountByEmailRepository;
+    this.setEmailConfirmationTokenRepository = setEmailConfirmationTokenRepository;
     this.sendEmailConfirmationRepository = sendEmailConfirmationRepository;
   }
-  async send(emailData: SendEmailConfirmationModel): Promise<void> {
-    const account = await this.loadAccountByEmailRepository.loadByEmail(emailData.email);
+  async send(email: string): Promise<void> {
+    const account = await this.loadAccountByEmailRepository.loadByEmail(email);
     if (account) {
-      await this.sendEmailConfirmationRepository.sendEmail(emailData);
+      const token = await this.setEmailConfirmationTokenRepository.setToken(email);
+      await this.sendEmailConfirmationRepository.sendEmail(email, token);
     }
   }
 }
