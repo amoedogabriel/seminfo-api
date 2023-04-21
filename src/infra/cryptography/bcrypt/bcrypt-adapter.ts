369a6c9edb27fac174d901785e507e8b95947917
Breno Gonzaga <contato@gonzaga.dev.br>
2023-04-20 20:42:56 -0400

    feat: add bcrypt compare adapter

--- src/infra/cryptography/bcrypt/bcrypt-adapter.ts
+++ src/infra/cryptography/bcrypt/bcrypt-adapter.ts
@@ -1,13 +1,18 @@
-import { Hasher } from '@data/protocols/cryptography';
+import { HashCompare, Hasher } from '@data/protocols/cryptography';
 import bcrypt from 'bcrypt';
 
-export class BCryptAdapter implements Hasher {
+export class BCryptAdapter implements Hasher, HashCompare {
   private readonly salt: number;
   constructor(salt: number) {
     this.salt = salt;
   }
   async hash(value: string): Promise<string> {
     const hashResult = await bcrypt.hash(value, this.salt);
+    return hashResult;
+  }
+
+  async compare(value: string, hash: string): Promise<boolean> {
+    const hashResult = await bcrypt.compare(value, hash);
     return hashResult;
   }
 }
