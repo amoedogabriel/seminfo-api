51babacbdd5cc6d0c27cabce320a8ff13de307a0
Breno Gonzaga <contato@gonzaga.dev.br>
2023-04-21 00:28:01 -0400

    feat: make login composition

---
+++ src/main/factories/login/login-factory.ts
@@ -0,0 +1,17 @@
+import { DbAuthentication } from '@data/use-cases';
+import { BCryptAdapter } from '@infra/cryptography/bcrypt';
+import { JwtAdapter } from '@infra/cryptography/jwt';
+import { AccountMongoRepository } from '@infra/db';
+import { LoginController } from '@presentation/controllers/login-controller';
+import { makeLoginValidationFactory } from '@main/factories/login';
+import env from '@main/config/env';
+
+export const makeLoginFactory = () => {
+  const salt = 12;
+  const authMongo = new AccountMongoRepository();
+  const hash = new BCryptAdapter(salt);
+  const encrypt = new JwtAdapter(env.secret);
+  const validation = makeLoginValidationFactory();
+  const authentication = new DbAuthentication(authMongo, authMongo, encrypt, hash);
+  return new LoginController(validation, authentication);
+};
