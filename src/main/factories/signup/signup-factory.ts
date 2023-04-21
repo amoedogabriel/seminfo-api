daa322f6eb8e72ab3c9caf9190b61fe4643a9b5d
Breno Gonzaga <contato@gonzaga.dev.br>
2023-04-20 20:43:35 -0400

    refactor: update SignUpController factory

--- src/main/factories/signup/signup-factory.ts
+++ src/main/factories/signup/signup-factory.ts
@@ -1,15 +1,19 @@
-import { DbAddAccount } from '@data/use-cases';
-import { BCryptAdapter } from '@infra/cryptography';
+import { DbAddAccount, DbAuthentication } from '@data/use-cases';
 import { AccountMongoRepository } from '@infra/db';
 import { SignUpControler } from '@presentation/controllers/signup-controller';
 import { makeSignupValidation } from '@main/factories/signup';
+import { JwtAdapter } from '@infra/cryptography/jwt';
+import { BCryptAdapter } from '@infra/cryptography/bcrypt';
 
 export const makeSignupController = (): SignUpControler => {
   const salt = 12;
   const hasher = new BCryptAdapter(salt);
+  const encrypter = new JwtAdapter(process.env.SECRET);
   const addAccountRepository = new AccountMongoRepository();
   const loadAccountByEmail = new AccountMongoRepository();
+  const updateAcessToken = new AccountMongoRepository();
   const addAcount = new DbAddAccount(hasher, addAccountRepository, loadAccountByEmail);
   const validation = makeSignupValidation();
-  return new SignUpControler(addAcount, validation);
+  const authentication = new DbAuthentication(loadAccountByEmail, updateAcessToken, encrypter, hasher);
+  return new SignUpControler(addAcount, validation, authentication);
 };
