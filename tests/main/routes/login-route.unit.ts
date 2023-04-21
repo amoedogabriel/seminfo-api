f59629722b4cea3f734442088a95ec9b07fc8805
Breno Gonzaga <contato@gonzaga.dev.br>
2023-04-21 00:49:59 -0400

    test: ensure Login routes return an access token on success

---
+++ tests/main/routes/login-route.unit.ts
@@ -0,0 +1,34 @@
+import { MongoHelper } from '@infra/helper';
+import app from '@main/config/app';
+import env from '@main/config/env';
+import { hash } from 'bcrypt';
+import { Collection } from 'mongodb';
+import request from 'supertest';
+
+let accountCollection: Collection;
+
+describe('Login Route', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(env.mongoURL);
+  });
+
+  afterAll(async () => {
+    await MongoHelper.disconnect();
+  });
+
+  beforeEach(async () => {
+    accountCollection = await MongoHelper.getCollection('account');
+    accountCollection.deleteMany({});
+  });
+
+  it('Should return an access token on success', async () => {
+    const password = await hash('@Valid123', 12);
+    await accountCollection.insertOne({
+      name: 'Breno Gonzaga',
+      email: 'any_email@mail.com',
+      password,
+    });
+    const loginAccount = { email: 'any_email@mail.com', password: '@Valid123' };
+    await request(app).post('/login').send(loginAccount).expect(200);
+  });
+});
