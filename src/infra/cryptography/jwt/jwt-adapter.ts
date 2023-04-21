d26b6c32c1194569b1874930d898887bbb8f729e
Breno Gonzaga <contato@gonzaga.dev.br>
2023-04-20 20:24:25 -0400

    feat: add jwtAdapter and move bcrypt to its own folder

---
+++ src/infra/cryptography/jwt/jwt-adapter.ts
@@ -0,0 +1,12 @@
+import { Encrypter } from '@data/protocols/cryptography';
+import jwt from 'jsonwebtoken';
+
+export class JwtAdapter implements Encrypter {
+  private readonly secret: string;
+  constructor(secret: string) {
+    this.secret = secret;
+  }
+  async encrypt(value: string): Promise<string> {
+    return jwt.sign(value, this.secret);
+  }
+}
