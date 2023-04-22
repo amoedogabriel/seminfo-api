e8ef73fc513a056ed80be73b571722e226b1a0e8
Breno Gonzaga <contato@gonzaga.dev.br>
2023-04-22 15:41:05 -0400

    fix: send email not working

--- src/infra/mail/node-mailer-adapter.ts
+++ src/infra/mail/node-mailer-adapter.ts
@@ -1,25 +1,28 @@
 import { SendEmailConfirmation } from '@data/protocols/mail';
 import nodemailer from 'nodemailer';
 import env from '@main/config/mail-env';
-const { host, port, user, pass } = env;
+const { host, port, user, pass, from } = env;
 
 export class NodeMailerAdapter implements SendEmailConfirmation {
   async sendEmail(email: string, token: string): Promise<void> {
     const transporter = nodemailer.createTransport({
       host: host,
       port: +port,
+      secure: true,
       auth: {
         user: user,
         pass: pass,
       },
     });
 
-    await transporter.sendMail({
-      from: '"Fred Foo 👻" <foo@example.com>', // sender address
-      to: email, // list of receivers
-      subject: 'Account Confirmation ✔', // Subject line
-      text: 'Account Confirmation', // plain text body
-      html: `<b>${token}</b>`, // html body
-    });
+    await transporter
+      .sendMail({
+        from: `${from} ${user}`, // sender address
+        to: email, // list of receivers
+        subject: 'Account Confirmation ✔', // Subject line
+        text: 'Account Confirmation', // plain text body
+        html: `<b>${token}</b>`, // html body
+      })
+      .catch((data) => console.log(data));
   }
 }
--- src/main/config/mail-env.ts
+++ src/main/config/mail-env.ts
@@ -3,4 +3,5 @@ export default {
   port: process.env.MAIL_PORT || 587,
   user: process.env.MAIL_USER || 'claudie.mills@ethereal.email',
   pass: process.env.MAIL_PASS || 'eN2WNCssXeZ37ChgjB',
+  from: process.env.FROM || 'Test EMAIL 👻',
 };
