ff8f318717b8266d185ac4eb8227bfabd4e96c06
Breno Gonzaga <contato@gonzaga.dev.br>
2023-04-22 20:36:57 -0400

    feat: ensure EmailConfirmationController calls ConfirmEmailTokenRepository with correct values

--- src/presentation/controllers/mail/email-confirmation-controller.ts
+++ src/presentation/controllers/mail/email-confirmation-controller.ts
@@ -1,21 +1,25 @@
 import { LoadAccountByEmailRepository } from '@data/protocols/db/account';
-import { ValidateConfirmationTokenRepository } from '@data/protocols/db/mail';
+import { ConfirmEmailTokenRepository, ValidateConfirmationTokenRepository } from '@data/protocols/db/mail';
 import { Controller, HttpRequest, HttpResponse } from '@presentation/protocols';
 
 export class EmailConfirmationController implements Controller {
   private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository;
   private readonly validateConfirmationTokenRepository: ValidateConfirmationTokenRepository;
+  private readonly confirmEmailTokenRepository: ConfirmEmailTokenRepository;
   constructor(
     loadAccountByEmailRepository: LoadAccountByEmailRepository,
-    validateConfirmationTokenRepository: ValidateConfirmationTokenRepository
+    validateConfirmationTokenRepository: ValidateConfirmationTokenRepository,
+    confirmEmailTokenRepository: ConfirmEmailTokenRepository
   ) {
     this.loadAccountByEmailRepository = loadAccountByEmailRepository;
     this.validateConfirmationTokenRepository = validateConfirmationTokenRepository;
+    this.confirmEmailTokenRepository = confirmEmailTokenRepository;
   }
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     const { email, confirmationToken } = httpRequest;
     await this.loadAccountByEmailRepository.loadByEmail(email);
     await this.validateConfirmationTokenRepository.validate(email, confirmationToken);
+    await this.confirmEmailTokenRepository.confirmEmail(email);
     return null;
   }
 }
