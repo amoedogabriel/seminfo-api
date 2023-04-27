734468f14f8d77c7bfbaeeff61cdd8d1cb0f75e0
Breno Gonzaga <contato@gonzaga.dev.br>
2023-04-27 16:52:23 -0400

    feat: ensure SignUpControler calls SendEmail with correct value

--- src/main/factories/signup/signup-factory.ts
+++ src/main/factories/signup/signup-factory.ts
@@ -1,24 +1,21 @@
 import { DbAddAccount } from '@data/use-cases/account';
-import { DbSetEmailConfirmationToken } from '@data/use-cases/mail';
+import { DbSetEmailConfirmationToken, ServiceSendEmail } from '@data/use-cases/mail';
 import { BCryptAdapter } from '@infra/cryptography/bcrypt';
 import { AccountMongoRepository } from '@infra/db/account';
 import { EmailMongoRepository } from '@infra/db/mail';
 import { SignUpControler } from '@presentation/controllers/account/signup-controller';
 import { makeSignupValidation } from '@main/factories/signup';
-import { NodeMailerAdapter } from '@infra/mail/node-mailer-adapter';
+import { MailNodeMailerProvider } from '@infra/mail/mail-nodemailer-provider';
 
 export const makeSignupController = (): SignUpControler => {
   const salt = 12;
   const hasher = new BCryptAdapter(salt);
   const accountMongo = new AccountMongoRepository();
   const sendMongo = new EmailMongoRepository();
-  const sendEmailConfirmation = new NodeMailerAdapter();
+  const sendEmailProvider = new MailNodeMailerProvider();
+  const sendEmail = new ServiceSendEmail(sendEmailProvider);
   const addAcount = new DbAddAccount(hasher, accountMongo, accountMongo);
   const validation = makeSignupValidation();
-  const setEmailConfirmation = new DbSetEmailConfirmationToken(
-    accountMongo,
-    sendMongo,
-    sendEmailConfirmation
-  );
-  return new SignUpControler(addAcount, validation, setEmailConfirmation);
+  const setEmailConfirmation = new DbSetEmailConfirmationToken(accountMongo, sendMongo);
+  return new SignUpControler(addAcount, validation, setEmailConfirmation, sendEmail);
 };
